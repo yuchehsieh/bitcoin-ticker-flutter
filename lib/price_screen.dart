22cca1b96cb95126472ace4a7ec43d761d1388a9
Murphy Hsieh <murphyhsieh@leandev.io>
2019-08-29 11:00:56 +0800

    material dropdown button

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +7,8 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String _selectedCurrency;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +45,25 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              hint: Text('Choose a Currency'),
+              value: _selectedCurrency,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              items: currenciesList.map((item) {
+                return DropdownMenuItem(
+                  value: item,
+                  child: Text(item),
+                );
+              }).toList(),
+              onChanged: (value) {
+                print(value);
+                setState(() {
+                  _selectedCurrency = value;
+                });
+              },
+            ),
           ),
         ],
       ),
